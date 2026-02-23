11211be72e5f6c14df88f1af43bda546f10cb109
Abel <abfafula@ucm.es>
2026-02-23 10:28:08 +0100

    Añadido prototipo de como se podria mover el palo al apuntar con el raton

--- src/game-objects/items/staffs-wands/staff.js
+++ src/game-objects/items/staffs-wands/staff.js
@@ -9,7 +9,8 @@ export default class Staff extends Phaser.GameObjects.Sprite {
         super(scene, 0, 0, 'palo');
         this.scene.add.existing(this);
         this.setVisible(false);
-        this.setAngle(-60);
+        //this.setOrigin(0,1)
+        //this.setAngle(-60);
         this.player = player;
         this.scene.input.mouse.disableContextMenu();
         this.scene.input.on('pointerdown', pointer => {
@@ -30,7 +31,9 @@ export default class Staff extends Phaser.GameObjects.Sprite {
 
     preUpdate(t,dt) {
         super.preUpdate(t,dt);
-        this.x = this.player.x - 10;
-        this.y = this.player.y;
+        this.x = this.player.x - 7;
+        this.y = this.player.y + 7.5;
+        const pointer = this.scene.input.activePointer;
+        this.rotation = Phaser.Math.Angle.Between(this.x,this.y - 1, pointer.x, pointer.y);
     }
 }
