2a208f828e3694f395614c5b8246b2ec1e62a9af
Abel <abfafula@ucm.es>
2026-03-05 14:23:24 +0100

    Cambiado: la manera en la que rota la varita sobre el player

--- src/game-objects/items/staffs-wands/staff.js
+++ src/game-objects/items/staffs-wands/staff.js
@@ -9,7 +9,7 @@ export default class Staff extends Phaser.GameObjects.Sprite {
         super(scene, 0, 0, 'palo');
         this.scene.add.existing(this);
         this.setVisible(false);
-        this.setOrigin(0.375,0.625)
+        this.setOrigin(0.21875,0.75)
         this.player = player;
         this.scene.input.mouse.disableContextMenu();
         this.scene.input.on('pointerdown', pointer => {
@@ -30,8 +30,8 @@ export default class Staff extends Phaser.GameObjects.Sprite {
 
     preUpdate(t,dt) {
         super.preUpdate(t,dt);
-        this.x = this.player.x - 5.5;
-        this.y = this.player.y + 6;
+        this.x = this.player.x;
+        this.y = this.player.y + 5;
         const pointer = this.scene.input.activePointer;
         this.rotation = Phaser.Math.Angle.Between(this.x, this.y, pointer.worldX, pointer.worldY) + Phaser.Math.DegToRad(45);
     }
