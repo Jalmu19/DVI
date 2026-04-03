d8f255ca16bd4c849e496b2592fadee9295d0c9d
Abel <abfafula@ucm.es>
2026-04-03 12:17:50 +0200

    Cambiado: el arrastre de caja solo se puede hacer en el eje en el que se empezo el movimiento

--- src/game-objects/items/cajas.js
+++ src/game-objects/items/cajas.js
@@ -13,6 +13,7 @@ export default class Cajas extends Phaser.Physics.Arcade.Sprite  {
 
         this.interactuable = true;
         this.pesoFactor = 0.4;
+        this.ejeBloqueado = null;
 
         this.setDrag(2000);
         // false para poder hacer el overlap con la bandera
@@ -25,11 +26,36 @@ export default class Cajas extends Phaser.Physics.Arcade.Sprite  {
     }
 
     interact(player) {
-        if(player.cursors.interact.isDown) {
-            player.body.velocity.x *= this.pesoFactor;
-            player.body.velocity.y *= this.pesoFactor;
-            this.setVelocity(player.body.velocity.x, player.body.velocity.y);
-            //TODO que solo se pueda mover en el eje en el que ha empezado
+         if (!this.ejeBloqueado) {
+            const diffX = Math.abs(player.x - this.x);
+            const diffY = Math.abs(player.y - this.y);
+
+            // Si la distancia en X es mayor que en Y, es que estamos a los lados (movimiento horizontal)
+            // Si la distancia en Y es mayor, es que estamos arriba/abajo (movimiento vertical)
+            if (diffX > diffY) this.ejeBloqueado = 'x';
+            else this.ejeBloqueado = 'y';
+        }
+
+        if (player.cursors.interact.isDown && this.ejeBloqueado) {
+            
+            if (this.ejeBloqueado === 'x') {
+                // Bloqueamos el movimiento vertical tanto del player como de la caja
+                player.body.velocity.y = 0; 
+                this.setVelocity(player.body.velocity.x * this.pesoFactor, 0);
+                
+                player.body.velocity.x *= this.pesoFactor;
+            } 
+            else if (this.ejeBloqueado === 'y') {
+                // Bloqueamos el movimiento horizontal tanto del player como de la caja
+                player.body.velocity.x = 0;
+                this.setVelocity(0, player.body.velocity.y * this.pesoFactor);
+                
+                player.body.velocity.y *= this.pesoFactor;
+            }
+        }
+
+        if (player.cursors.interact.isUp) {
+            this.ejeBloqueado = null;
         }
     }
 
