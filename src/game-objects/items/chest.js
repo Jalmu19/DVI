eefb2ae697a8c06fab783f826dacd7911b435384
yo262 <irodri15@ucm.es>
2026-04-17 17:09:04 +0200

    nombres items del cofre en español

--- src/game-objects/items/chest.js
+++ src/game-objects/items/chest.js
@@ -43,7 +43,7 @@ export default class Chest extends Phaser.Physics.Arcade.Sprite {
             player.acquireFromChest(this.itemIn);
 
             // Obtenemos el nombre del item o un genérico si no tiene
-            let itemName = this.itemIn ? this.itemIn.id : "Objeto desconocido";
+            let itemName = this.itemIn ? this.itemIn.name : "Objeto desconocido";
             this.scene.dialogo("Has recibido: " + itemName);
         }
     }
