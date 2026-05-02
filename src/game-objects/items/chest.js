5bd16f6d459e5c603654a5aa2bc86aed5c671fac
sesils <cecili03@ucm.es>
2026-05-02 19:48:21 +0200

    Añadido tutorial de cambiar hechizos

--- src/game-objects/items/chest.js
+++ src/game-objects/items/chest.js
@@ -45,8 +45,60 @@ export default class Chest extends Phaser.Physics.Arcade.Sprite {
             // Obtenemos el nombre del item o un genérico si no tiene
             let itemName = this.itemIn ? this.itemIn.name : "Objeto desconocido";
             this.scene.dialogo("Has recibido: " + itemName);
-
+            
+            if(itemName === "hechizo de hielo"){
+                this.scene.time.delayedCall(4000, () => {
+                    this.mostrarTutorialCambiarHechizos();
+                });
+            }
             this.scene.sound.add('chestSound').play();
         }
     }
+
+    mostrarTutorialCambiarHechizos(){
+        const contenedor = this.scene.add.container(320 / 2 + 70, 180 / 2 + 60).setScrollFactor(0);
+        contenedor.setAlpha(0);
+
+        this.scene.tweens.add({
+            targets: contenedor,
+            alpha: 1,         
+            duration: 500,    
+            ease: 'Power2'  
+        });
+
+        const fondo = this.scene.add.rectangle(0, 0, 170, 30, 0x000080, 0.8);
+        fondo.setStrokeStyle(2, 0xffffff);
+
+        const mensaje = this.scene.add.text(0, 1, 'Cambiar hechizos : Tabulador', {
+            fontSize: '10px',
+            fill: '#fff',
+        }).setOrigin(0.5);
+
+        const btnCerrar = this.scene.add.text(81, -15, 'X', {
+            fontSize: '10px',
+            fill: '#000000',
+            backgroundColor: '#ff96ea'
+        })
+        .setOrigin(0.5)
+        .setPadding(2)
+
+       const zonaClick = this.scene.add.zone(311, 135, 10, 10); 
+        zonaClick.setInteractive({ useHandCursor: true });
+        zonaClick.setScrollFactor(0);
+        zonaClick.on('pointerdown', () => {
+            this.scene.tweens.add({
+                targets: contenedor,
+                alpha: 0,
+                duration: 300,
+                onComplete: () => {
+                    contenedor.destroy();
+                    zonaClick.destroy();
+                }
+            });
+        });
+
+        contenedor.add([fondo, mensaje, btnCerrar]);
+        contenedor.setDepth(100);
+    }
+
 }
