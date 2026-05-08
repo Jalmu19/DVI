f6d649ad3a1117202e5b1b790b4bddfb6ca5daba
Abel <abfafula@ucm.es>
2026-05-09 00:59:05 +0200

    Arreglado: funcionan los creditos

--- src/credits.js
+++ src/credits.js
@@ -6,8 +6,8 @@ export default class CreditsScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, 'Pulsa ESC para volver', { 
-            fontSize: '18px', 
+        this.add.text(10, 10, 'Pulsa ESC para volver', { 
+            fontSize: 10, 
             fill: '#aaaaaa' 
         });
 
@@ -15,51 +15,44 @@ export default class CreditsScene extends Phaser.Scene {
             this.scene.start('mainmenu'); 
         });
 
-        const creditos = `
-            [ERTUTAS]
-            Programación, UI, Sprites
-
-            [Sesils]
-            Mapas interiores, música, progamación
-
-            [Jalmu19]
-            Lo que sea
-
-            [yo262]
-            Lo que sea
-
-            ==== ASSETS EXTERNOS ===
-            TILESETS
-                Lo que sea
-            
-            SPRITES
-                Lo que sea
-
-            ==== HERRAMIENTAS ====
-            Desarrollado con Phaser 3
-            Sprites diseñados con Aseprite
-            Mapas creados con Tiled
-            Música creada con ---Lo que sea---
-
-            ¡GRACIAS POR JUGAR!
-
-
-
-
-
-
-
-        `;
+        const creditos = [
+            "=== EQUIPO ===",
+            "[ERTUTAS]",
+            "Programación, UI, Sprites",
+            "",
+            "[Sesils]",
+            "Mapas interiores, música, progamación",
+            "",
+            "[Jalmu19]",
+            "Lo que sea",
+            "",
+            "[yo262]",
+            " Lo que sea",
+            "",
+            "==== ASSETS EXTERNOS ===",
+            "TILESETS",
+            "Lo que sea",
+            "",
+            "SPRITES",
+            "Lo que sea",
+            "",
+            "==== HERRAMIENTAS ====",
+            "Desarrollado con Phaser 3",
+            "Sprites diseñados con Aseprite",
+            "Mapas creados con Tiled",
+            "Música creada con ---Lo que sea---",
+            "",
+            "¡GRACIAS POR JUGAR!"
+        ];
 
         let textoAnimado = this.add.text(SCENE.WIDTH / 2, SCENE.HEIGHT + 100, creditos, { 
-            fontSize: '24px', 
-            fill: '#cccccc',
-            align: 'center'
-        }).setOrigin(0.5, 0.5).setAlign('center');
+            fontSize: 12.5, 
+            fill: '#cccccc'
+        }).setOrigin(0.5).setAlign('center');
 
         this.tweens.add({
-            targets: textoAnimado.height,
-            y: -textoAnimado - 50,
+            targets: textoAnimado,
+            y: -textoAnimado.height,
             duration: 15000,
             repeat: -1
         });
