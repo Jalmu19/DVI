078a10e5c8ca12507c0eb2ba4f9be3215d40bb2c
Abel <abfafula@ucm.es>
2026-05-14 18:57:52 +0200

    Añadido: ventanitas para mostrar info de los objetos en inventario

--- src/ui/inventoryScene.js
+++ src/ui/inventoryScene.js
@@ -29,6 +29,23 @@ export default class InventoryScene extends Phaser.Scene {
         this.positions = [];
         this.add.image(this.cameras.main.centerX, this.cameras.main.centerY,'inventoryBackground');     //Pinta el fondo
         this.paintItems();
+        this.paintToolTip();
+    }
+
+    paintToolTip() {
+        this.tooltipText = this.add.text(4, 4, '', {
+            fontStyle: '"Courier New", Courier, monospace',
+            fontSize: '2px',
+            color: '#ffffff',
+            wordWrap: { width: 120 },
+            resolution: 2
+        });
+
+        this.tooltipBg = this.add.graphics();
+
+        this.tooltip = this.add.container(0, 0, [this.tooltipBg, this.tooltipText]);
+        this.tooltip.setDepth(10);
+        this.tooltip.setVisible(false);
     }
 
     paintItems() {
@@ -62,8 +79,51 @@ export default class InventoryScene extends Phaser.Scene {
             });
 
             //Efecto de seleccionar el item cuando el raton esta encima
-            img.on('pointerover', () => img.setTint(0xcccccc));
-            img.on('pointerout', () => img.clearTint());
+            img.on('pointerover', () => {
+                img.setTint(0xcccccc);
+
+                // Mostrar info del objeto
+                let objetoTexto = `** ${item.name || 'Objeto'} **\n`;
+
+                if (item.id === 'berry') {
+                    objetoTexto += `Una baya que restaura un poco de salud.\nCura: Medio corazón`;
+                } 
+                else if (item.id.includes('staff')) {
+                    if(item.id === 'staff-lago') objetoTexto += `Un báculo. \nBonus Daño : 0.5`;
+                    else objetoTexto += `Un báculo viejo. \nBonus Daño : 0`;
+                }
+
+                this.tooltipText.setText(objetoTexto);
+
+                let textBounds = this.tooltipText.getBounds();
+                this.tooltipBg.clear();
+                this.tooltipBg.fillStyle(0x000080, 0.85); // Negro con 85% de opacidad
+                this.tooltipBg.lineStyle(1, 0xffffff, 1);
+
+                this.tooltipBg.fillRoundedRect(0, 0, textBounds.width + 8, textBounds.height + 8, 2);
+                this.tooltipBg.strokeRoundedRect(0, 0, textBounds.width + 8, textBounds.height + 8, 2);
+
+                let posX = img.x + 20; 
+                let posY = img.y - 10; 
+
+                // Evitar que se salga de la pantalla por la derecha
+                if (posX + textBounds.width + 8 > this.scale.width) {
+                    posX = img.x - textBounds.width - 20; // Si no cabe, lo ponemos a la izquierda del objeto
+                }
+                // Evitar que se salga por abajo
+                if (posY + textBounds.height + 8 > this.scale.height) {
+                    posY = this.scale.height - textBounds.height - 12;
+                }
+
+                this.tooltip.setPosition(posX, posY);
+
+                this.tooltip.setVisible(true);
+
+            });
+            img.on('pointerout', () => {
+                img.clearTint();
+                this.tooltip.setVisible(false);
+            });
         });
     }
 
